0b7a2fff72a0fa5b93b91f131dc57d45c1f7c4b0
Santiago Villabona <88673966+MeperDonas@users.noreply.github.com>
2024-09-20 11:29:39 -0500

    eliminacion del endpoint de allProduct

--- controller/controller.js
+++ controller/controller.js
@@ -76,23 +76,3 @@ export const insertProductos = async (req, res) => {
   }
 };
 
-export const AllProducts = async (req, res) => {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    console.log("Iniciando la conexión");
-    const rows = await conn.query("SELECT * FROM PRODUCT;");
-    console.log("Datos obtenidos: ", rows); // Verifica qué datos se están obteniendo
-    if (rows.length > 0) {
-      res.json(rows);
-    } else {
-      res.status(404).json({ status: "No se encontraron productos" });
-    }
-  } catch (err) {
-    res.status(500).json({ status: "Error en la base de datos" });
-    console.log("Error en la base de datos", err);
-  } finally {
-    if (conn) conn.end();
-  }
-};
-
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 
 import {
-  getProducts, insertProductos, AllProducts
+  getProducts, insertProductos,
 } from "../controller/controller.js";
 
 //cambiar el nombre del endpoint
 
 export const router = Router();
 router.get("/product/:IdProduct", getProducts); 
-router.get("/allProducts", AllProducts);
 router.post("/product/compra", insertProductos);
 
 
